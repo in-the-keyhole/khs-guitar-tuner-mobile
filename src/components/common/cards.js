260170a44a03e5f7cdcccafe35302fcddab604b5
Colin Smith <colinsmith1998@gmail.com>
2018-07-24 13:41:03 -0500

    Created fetch API call

--- src/components/common/cards.js
+++ src/components/common/cards.js
@@ -2,14 +2,29 @@ import React from 'react';
 import { StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { COLORS } from 'constants/styles';
-import TUNINGS from 'data/data.js';
 
 class Cards extends React.Component{
+	constructor() {
+        super();
+        this.state = {
+            tunings: []
+        };
+	}
+	
+	componentWillMount() {
+        fetch( 'http://www.khsguitartuner.com/api-tunings' )
+            .then( results => { return results.json(); } )
+            .then( data => {
+                this.setState( { tunings: data._embedded.tunings } );
+            } )
+            .catch(( error ) => { console.log( error ) } );
+    }
+	
     render() {
         return (
             <Card containerStyle={styles.card} >
             {
-              TUNINGS.map((t, i) => {
+              this.state.tunings.map((t, i) => {
                 return (
                   <ListItem
                     key={i}
