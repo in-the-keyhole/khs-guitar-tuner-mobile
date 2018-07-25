2bf57bcd124339a9639228fa8ec34779db2f6662
Colin Smith <colinsmith1998@gmail.com>
2018-07-25 11:11:21 -0500

    Fixing the Letters on the frets

--- src/components/screens/detail-screen.js
+++ src/components/screens/detail-screen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, View, StyleSheet, Text } from 'react-native';
+import { Image, View, StyleSheet, Text, ImageBackground } from 'react-native';
 import {Header} from 'components/common/header.js';
 import { Actions } from 'react-native-router-flux';
 import {COLORS} from 'constants/styles.js';
@@ -18,17 +18,43 @@ class Detail extends React.Component {
 
     render(){
 		let splitNotes = global.notes.split(',');
+		if(splitNotes[0].length == 1){
+			splitNotes[0] += ' ';
+			splitNotes[0] += ' ';
+		}
+		if(splitNotes[1].length == 1){
+			splitNotes[1] += ' ';
+			splitNotes[1] += ' ';
+		}
+		if(splitNotes[2].length == 1){
+			splitNotes[2] += ' ';
+			splitNotes[2] += ' ';
+		}
+		if(splitNotes[3].length == 1){
+			splitNotes[3] += ' ';
+			splitNotes[3] += ' ';
+		}
+		if(splitNotes[4].length == 1){
+			splitNotes[4] += ' ';
+			splitNotes[4] += ' ';
+		}
+		if(splitNotes[5].length == 1){
+			splitNotes[5] += ' ';
+			splitNotes[5] += ' ';
+		}
         return(
             <View style={styles.Detail}>
                 <Header title={global.description + ' Guitar Tuning'} showAbout={false} gotoHome={this.gotoHome} />
                 <View style={styles.Fret}>
                     <Image style={styles.Fret} source={require('./images/fret2.jpg')} />
-					<Text style={styles.Text1}>{splitNotes[0]}</Text>
-					<Text style={styles.Text2}>{splitNotes[1]}</Text>
-					<Text style={styles.Text3}>{splitNotes[2]}</Text>
-					<Text style={styles.Text4}>{splitNotes[3]}</Text>
-					<Text style={styles.Text5}>{splitNotes[4]}</Text>
-					<Text style={styles.Text6}>{splitNotes[5]}</Text>
+						<View style={{flex:1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+							<Text style={styles.Text}>{splitNotes[0].toUpperCase()}</Text>
+							<Text style={styles.Text}>{splitNotes[1].toUpperCase()}</Text>
+							<Text style={styles.Text}>{splitNotes[2].toUpperCase()}</Text>
+							<Text style={styles.Text}>{splitNotes[3].toUpperCase()}</Text>
+							<Text style={styles.Text}>{splitNotes[4].toUpperCase()}</Text>
+							<Text style={styles.Text}>{splitNotes[5].toUpperCase()}</Text>
+						</View>
                 </View>
             </View>
         );
@@ -46,66 +72,13 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
-	Text1: {
-		position: 'absolute',
-		top: 200,
-		left: 100,
-		fontSize: 50,
-		marginTop: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
+	Text: {
+		fontWeight: 'bold',
+		height: 450,
+		padding: 5,
+		fontSize: 30,
 		color: 'rgb(255,255,255)',
 	},
-	Text2: {
-		position: 'absolute',
-		top: 200,
-		left: 140,
-		fontSize: 50,
-		marginTop: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-		color: 'rgb(255,255,255)',
-	},
-	Text3: {
-		position: 'absolute',
-		top: 200,
-		left: 175,
-		fontSize: 50,
-		marginTop: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-		color: 'rgb(255,255,255)',
-	},
-	Text4: {
-		position: 'absolute',
-		top: 200,
-		left: 210,
-		fontSize: 50,
-		marginTop: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-		color: 'rgb(255,255,255)',
-	},
-	Text5: {
-		position: 'absolute',
-		top: 200,
-		left: 250,
-		fontSize: 50,
-		marginTop: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-		color: 'rgb(255,255,255)',
-	},
-	Text6: {
-		position: 'absolute',
-		top: 200,
-		left: 290,
-		fontSize: 50,
-		marginTop: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-		color: 'rgb(255,255,255)',
-	}
 });
 
 export {Detail};
