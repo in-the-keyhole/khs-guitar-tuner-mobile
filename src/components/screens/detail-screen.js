6a1ddcc6f432ccfaa30b458fb2f5997a808da521
Braden Niswonger <bradenniswonger@gmail.com>
2018-07-25 16:46:00 -0500

    detail screen update

--- src/components/screens/detail-screen.js
+++ src/components/screens/detail-screen.js
@@ -22,88 +22,93 @@ class Detail extends React.Component {
     }
 
     playSound = async (note) => {
-        await this.audioPlayer.unloadAsync();
-        if(note === 'a1'){
-            this.setState({note: 'a1', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/a-1.mp3'));
-        }else if(note === 'a4'){
-            this.setState({note: 'a4', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/a-4.mp3'));
-        }else if(note === 'a#4'){
-            this.setState({note: 'a#4', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/a-sharp-4.mp3'));
-        }else if(note === 'b4'){
-            this.setState({note: 'b4', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/b-4.mp3'));
-        }else if(note === 'c0'){
-            this.setState({note: 'c0', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/c-0.mp3'));
-        }else if(note === 'c2'){
-            this.setState({note: 'c2', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/c-2.mp3'));
-        }else if(note === 'c3'){
-            this.setState({note: 'c3', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/c-3.mp3'));
-        }else if(note === 'c4'){
-            this.setState({note: 'c4', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/c-4.mp3'));
-        }else if(note === 'c5'){
-            this.setState({note: 'c5', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/c-5.mp3'));
-        }else if(note === 'c#2'){
-            this.setState({note: 'c#2', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/c-sharp-2.mp3'));
-        }else if(note === 'd0'){
-            this.setState({note: 'd0', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/d-0.mp3'));
-        }else if(note === 'd1'){
-            this.setState({note: 'd1', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/d-1.mp3'));
-        }else if(note === 'd2'){
-            this.setState({note: 'd2', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/d-2.mp3'));
-        }else if(note === 'd5'){
-            this.setState({note: 'd5', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/d-5.mp3'));
-        }else if(note === 'd#0'){
-            this.setState({note: 'd#0', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/d-sharp-0.mp3'));
-        }else if(note === 'd#5'){
-            this.setState({note: 'd#5', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/d-sharp-5.mp3'));
-        }else if(note === 'e0'){
-            this.setState({note: 'e0', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/e-0.mp3'));
-        }else if(note === 'e5'){
-            this.setState({note: 'e5', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/e-5.mp3'));
-        }else if(note === 'f1'){
-            this.setState({note: 'f1', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/f-1.mp3'));
-        }else if(note === 'f3'){
-            this.setState({note: 'f3', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/f-3.mp3'));
-        }else if(note === 'f#3'){
-            this.setState({note: 'f#3', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/f-sharp-3.mp3'));
-        }else if(note === 'g1'){
-            this.setState({note: 'g1', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/g-1.mp3'));
-        }else if(note === 'g3'){
-            this.setState({note: 'g3', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/g-3.mp3'));
-        }else if(note === 'g#1'){
-            this.setState({note: 'g#1', playing: true});
-            await this.audioPlayer.loadAsync(require('./audio/g-sharp-1.mp3'));
-        }
+        try {
+            await this.audioPlayer.unloadAsync();
+            if(note === 'a1'){
+                this.setState({note: 'a1', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/a-1.mp3'));
+            }else if(note === 'a4'){
+                this.setState({note: 'a4', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/a-4.mp3'));
+            }else if(note === 'a#4'){
+                this.setState({note: 'a#4', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/a-sharp-4.mp3'));
+            }else if(note === 'b4'){
+                this.setState({note: 'b4', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/b-4.mp3'));
+            }else if(note === 'c0'){
+                this.setState({note: 'c0', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/c-0.mp3'));
+            }else if(note === 'c2'){
+                this.setState({note: 'c2', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/c-2.mp3'));
+            }else if(note === 'c3'){
+                this.setState({note: 'c3', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/c-3.mp3'));
+            }else if(note === 'c4'){
+                this.setState({note: 'c4', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/c-4.mp3'));
+            }else if(note === 'c5'){
+                this.setState({note: 'c5', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/c-5.mp3'));
+            }else if(note === 'c#2'){
+                this.setState({note: 'c#2', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/c-sharp-2.mp3'));
+            }else if(note === 'd0'){
+                this.setState({note: 'd0', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/d-0.mp3'));
+            }else if(note === 'd1'){
+                this.setState({note: 'd1', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/d-1.mp3'));
+            }else if(note === 'd2'){
+                this.setState({note: 'd2', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/d-2.mp3'));
+            }else if(note === 'd5'){
+                this.setState({note: 'd5', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/d-5.mp3'));
+            }else if(note === 'd#0'){
+                this.setState({note: 'd#0', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/d-sharp-0.mp3'));
+            }else if(note === 'd#5'){
+                this.setState({note: 'd#5', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/d-sharp-5.mp3'));
+            }else if(note === 'e0'){
+                this.setState({note: 'e0', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/e-0.mp3'));
+            }else if(note === 'e5'){
+                this.setState({note: 'e5', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/e-5.mp3'));
+            }else if(note === 'f1'){
+                this.setState({note: 'f1', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/f-1.mp3'));
+            }else if(note === 'f3'){
+                this.setState({note: 'f3', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/f-3.mp3'));
+            }else if(note === 'f#3'){
+                this.setState({note: 'f#3', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/f-sharp-3.mp3'));
+            }else if(note === 'g1'){
+                this.setState({note: 'g1', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/g-1.mp3'));
+            }else if(note === 'g3'){
+                this.setState({note: 'g3', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/g-3.mp3'));
+            }else if(note === 'g#1'){
+                this.setState({note: 'g#1', playing: true});
+                await this.audioPlayer.loadAsync(require('./audio/g-sharp-1.mp3'));
+            }else{
+                alert('Error: Audio does not exist');
+            }
 
-        await this.audioPlayer.setIsLoopingAsync(true);
-        await this.audioPlayer.playAsync();
+            await this.audioPlayer.setIsLoopingAsync(true);
+            await this.audioPlayer.playAsync();
+        } catch (err) {
         
+        }
     }
 
     stopSound = async () => {
-        this.setState({playing: false});
+        this.setState({note: '', playing: false});
         await this.audioPlayer.stopAsync();
     }
 
@@ -160,6 +165,7 @@ const styles = StyleSheet.create({
         
     },
     Fret: {
+        marginTop: -15,
         justifyContent: 'center',
         alignItems: 'center'
     },
@@ -182,8 +188,9 @@ const styles = StyleSheet.create({
 		color: COLORS.BLUE.BLUE,
 	},
 	Button: {
-		top: 10,
-		height: 50,
+        marginTop: 20,
+		bottom: 3,
+		height: 35,
 		width: 100,
 		borderRadius: 10,
 	},
@@ -192,7 +199,7 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		fontWeight: 'bold',
 		height: 50,
-		fontSize: 23,
+		fontSize: 16,
 		color: COLORS.WHITE.WHITE,
 	}
 });
