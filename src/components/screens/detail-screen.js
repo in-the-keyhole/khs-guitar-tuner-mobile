3704a88378d5d817d167e8c5ccdc7085df504d7f
Braden Niswonger <bradenniswonger@gmail.com>
2018-07-25 14:07:53 -0500

    audio on string letters

--- src/components/screens/detail-screen.js
+++ src/components/screens/detail-screen.js
@@ -10,6 +10,8 @@ class Detail extends React.Component {
         super(props);
         this.state ={
             tunings: this.props.tunings,
+            note: '',
+            playing: false
         }
         this.audioPlayer = new Audio.Sound();
     }
@@ -20,10 +22,57 @@ class Detail extends React.Component {
 
     playSound = async (note) => {
         await this.audioPlayer.unloadAsync();
-        if(note === 'e0'){
+        if(note === 'a1'){
+            await this.audioPlayer.loadAsync(require('./audio/a-1.mp3'));
+        }else if(note === 'a4'){
+            await this.audioPlayer.loadAsync(require('./audio/a-4.mp3'));
+        }else if(note === 'a#4'){
+            await this.audioPlayer.loadAsync(require('./audio/a-sharp-4.mp3'));
+        }else if(note === 'b4'){
+            await this.audioPlayer.loadAsync(require('./audio/b-4.mp3'));
+        }else if(note === 'c0'){
+            await this.audioPlayer.loadAsync(require('./audio/c-0.mp3'));
+        }else if(note === 'c2'){
+            await this.audioPlayer.loadAsync(require('./audio/c-2.mp3'));
+        }else if(note === 'c3'){
+            await this.audioPlayer.loadAsync(require('./audio/c-3.mp3'));
+        }else if(note === 'c4'){
+            await this.audioPlayer.loadAsync(require('./audio/c-4.mp3'));
+        }else if(note === 'c5'){
+            await this.audioPlayer.loadAsync(require('./audio/c-5.mp3'));
+        }else if(note === 'c#2'){
+            await this.audioPlayer.loadAsync(require('./audio/c-sharp-2.mp3'));
+        }else if(note === 'd0'){
+            await this.audioPlayer.loadAsync(require('./audio/d-0.mp3'));
+        }else if(note === 'd1'){
+            await this.audioPlayer.loadAsync(require('./audio/d-1.mp3'));
+        }else if(note === 'd2'){
+            await this.audioPlayer.loadAsync(require('./audio/d-2.mp3'));
+        }else if(note === 'd5'){
+            await this.audioPlayer.loadAsync(require('./audio/d-5.mp3'));
+        }else if(note === 'd#0'){
+            await this.audioPlayer.loadAsync(require('./audio/d-sharp-0.mp3'));
+        }else if(note === 'd#5'){
+            await this.audioPlayer.loadAsync(require('./audio/d-sharp-5.mp3'));
+        }else if(note === 'e0'){
+            this.setState({note: 'e0'});
             await this.audioPlayer.loadAsync(require('./audio/e-0.mp3'));
+        }else if(note === 'e5'){
+            await this.audioPlayer.loadAsync(require('./audio/e-5.mp3'));
+        }else if(note === 'f1'){
+            await this.audioPlayer.loadAsync(require('./audio/f-1.mp3'));
+        }else if(note === 'f3'){
+            await this.audioPlayer.loadAsync(require('./audio/f-3.mp3'));
+        }else if(note === 'f#3'){
+            await this.audioPlayer.loadAsync(require('./audio/f-sharp-3.mp3'));
+        }else if(note === 'g1'){
+            await this.audioPlayer.loadAsync(require('./audio/g-1.mp3'));
+        }else if(note === 'g3'){
+            await this.audioPlayer.loadAsync(require('./audio/g-3.mp3'));
+        }else if(note === 'g#1'){
+            await this.audioPlayer.loadAsync(require('./audio/g-sharp-1.mp3'));
         }
-        await this.audioPlayer.loadAsync(require('./audio/e-0.mp3'));
+
         await this.audioPlayer.setIsLoopingAsync(true);
         await this.audioPlayer.playAsync();
         
@@ -45,17 +94,16 @@ class Detail extends React.Component {
         return(
             <View style={styles.Detail}>
                 <Header title={global.description + ' Guitar Tuning'} showAbout={false} gotoHome={this.gotoHome} />
-                <Button onPress={() => this.playSound(note0)} title={'Play'}/>
                 <Button onPress={() => this.stopSound()} title={'Stop'}/>
                 <View style={styles.Fret}>
                     <Image style={styles.Fret} source={require('./images/fret2.jpg')} />
 						<View style={{flex:1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
-							<Text style={styles.Text}>{splitNotes[0].toUpperCase()}</Text>
-							<Text style={styles.Text}>{splitNotes[1].toUpperCase()}</Text>
-							<Text style={styles.Text}>{splitNotes[2].toUpperCase()}</Text>
-							<Text style={styles.Text}>{splitNotes[3].toUpperCase()}</Text>
-							<Text style={styles.Text}>{splitNotes[4].toUpperCase()}</Text>
-							<Text style={styles.Text}>{splitNotes[5].toUpperCase()}</Text>
+							<Text style={styles.Text} onPress={() => this.playSound(note0)}>{splitNotes[0].toUpperCase()}</Text>
+							<Text style={styles.Text} onPress={() => this.playSound(note1)}>{splitNotes[1].toUpperCase()}</Text>
+							<Text style={styles.Text} onPress={() => this.playSound(note2)}>{splitNotes[2].toUpperCase()}</Text>
+							<Text style={styles.Text} onPress={() => this.playSound(note3)}>{splitNotes[3].toUpperCase()}</Text>
+							<Text style={styles.Text} onPress={() => this.playSound(note4)}>{splitNotes[4].toUpperCase()}</Text>
+							<Text style={styles.Text} onPress={() => this.playSound(note5)}>{splitNotes[5].toUpperCase()}</Text>
 						</View>
                 </View>
             </View>
