46ea3cb24e6f7d17cc2dc4e7ec4914f51a120654
Anna <alscarbo@ksu.edu>
2018-07-26 13:47:14 -0500

    merge issue fix

--- src/components/screens/detail-screen.js
+++ src/components/screens/detail-screen.js
@@ -210,13 +210,9 @@ const styles = StyleSheet.create({
 	BottomText: {
 		top: -50,
 		fontWeight: 'bold',
-<<<<<<< HEAD
 		fontSize: 17,
-=======
         fontSize: 15,
         top: -50
->>>>>>> abef5c864140827f4682a7b65a33fb78e4766803
-	}
 });
 
 export {Detail};
