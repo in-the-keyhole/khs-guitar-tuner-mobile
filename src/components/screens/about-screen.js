4e075a83ad85e0eb8bcd3c215fe8fcd769ac834e
Braden Niswonger <bradenniswonger@gmail.com>
2018-07-31 16:41:07 -0500

    update about screen

--- src/components/screens/about-screen.js
+++ src/components/screens/about-screen.js
@@ -11,6 +11,7 @@ class About extends React.Component {
         return (
             <View style={styles.About}>
                 <Header title="About the App" gotoHome={this.gotoHome} showAbout={false}/>
+                <Text style={styles.Header}>KHS Guitar Tuner</Text>
                 <Text style={styles.Text}>Click on a tuning to listen and follow the instructions to begin tuning your guitar.</Text>
                 <Text style={styles.Text}>This mobile app was create by Keyhole Software using React Native elements and components.</Text>
                 <Text style={styles.Text}>Visit our web application at: www.khsguitartuner.com</Text>
@@ -27,6 +28,14 @@ const styles = StyleSheet.create({
     },
     Text: {
         fontSize: 20,
+        marginTop: 20,
+        marginLeft: 20,
+        marginRight: 20,
+        color: '#808080'
+    },
+    Header: {
+        fontSize: 40,
+        fontWeight: 'bold',
         marginTop: 20,
         marginLeft: 20,
         marginRight: 20
